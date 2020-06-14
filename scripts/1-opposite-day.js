59ae9ddb20ccc92411ee064ab716c97a416f8340
fatih <fatihkarakus@hotmail.com>
2020-06-14 21:04:47 +0200

    Solved script1 and 2

--- scripts/1-opposite-day.js
+++ scripts/1-opposite-day.js
@@ -1,28 +1,29 @@
 // fill in the blanks
 
-console.log('--- loading: opposite day');
+console.log("--- loading: opposite day");
 
 function oppositeDayHandler() {
-  'use strict';
+  "use strict";
   debugger;
 
   // read inputs from user
-  const showTheNumber = _(`do you want to see a random number?`);
+  const showTheNumber = confirm(`do you want to see a random number?`);
 
   // before moving on ... type check!
-  if (typeof showTheNumber !== 'boolean') { throw new TypeError(); }
+  if (typeof showTheNumber !== "boolean") {
+    throw new TypeError();
+  }
 
   // perform core logic
-  const oppositeDay = _;
-  const message = _ ? (100 * Math.random()) : 'as you wish.  good day!';
+  const oppositeDay = showTheNumber;
+  const message = oppositeDay ? 100 * Math.random() : "as you wish.  good day!";
 
   // alert result for the user
-  alert(_);
+  alert(message);
 
   // log action for the developer
-  console.log('\n--- opposite day ---');
-  console.log('showTheNumber:', typeof showTheNumber, '\n', showTheNumber);
-  console.log('oppositeDay:', typeof oppositeDay, '\n', oppositeDay);
-  console.log('message:', typeof message, '\n', message);
-
+  console.log("\n--- opposite day ---");
+  console.log("showTheNumber:", typeof showTheNumber, "\n", showTheNumber);
+  console.log("oppositeDay:", typeof oppositeDay, "\n", oppositeDay);
+  console.log("message:", typeof message, "\n", message);
 }
--- scripts/2-honesty-test.js
+++ scripts/2-honesty-test.js
@@ -1,32 +1,37 @@
 // fill in the blanks
 
-console.log('--- loading: honesty test');
+console.log("--- loading: honesty test");
 
 function honestyTestHandler() {
-  'use strict';
+  "use strict";
   debugger;
 
   // read inputs from user
-  const userString = _(`Please enter something:`);
+  const userString = prompt(`Please enter something:`);
 
-  const confirmation = _(`Did you enter something? \nyes: "ok" \nno: "cancel"`);
+  const confirmation = confirm(
+    `Did you enter something? \nyes: "ok" \nno: "cancel"`
+  );
 
   // before moving on ... type check!
-  if (typeof userString !== 'string') { throw new TypeError(); }
-  if (typeof confirmation !== 'boolean') { throw new TypeError(); }
+  if (typeof userString !== "string") {
+    throw new TypeError();
+  }
+  if (typeof confirmation !== "boolean") {
+    throw new TypeError();
+  }
 
   // perform core logic
-  const didEnterSomething = userString ? _ : _;
+  const didEnterSomething = userString ? false : true;
   const isALiar = confirmation === didEnterSomething;
-  const message = `You are a ${_ ? 'liar' : 'mensch'}!`;
+  const message = `You are a ${isALiar ? "liar" : "mensch"}!`;
 
   // alert result for the user
-  alert(_);
+  alert(message);
 
   // log action for the developer
-  console.log('\n--- honesty test ---');
-  console.log('userString:', typeof userString, '\n', userString);
-  console.log('confirmation:', typeof confirmation, '\n', confirmation);
-  console.log('message:', typeof message, '\n', message);
-
+  console.log("\n--- honesty test ---");
+  console.log("userString:", typeof userString, "\n", userString);
+  console.log("confirmation:", typeof confirmation, "\n", confirmation);
+  console.log("message:", typeof message, "\n", message);
 }
